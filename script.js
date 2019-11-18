65fadf327ca4576b0817603e961afccb8733e2de
Joe Bailey <baileyrva@gmail.com>
2019-11-18 17:57:14 -0500

    JS on checkboxes complete

--- script.js
+++ script.js
@@ -1,40 +1,90 @@
-function create(){
+var instructions =
+  "Your password must be at least 8 characters and must satisfy the four check boxes. Once completed, you should copy to Clipboard. Additionally, you can use the slider to create a custom password for you!";
 
-    let complexity = document.getElementById("slider").value;
+alert(instructions);
 
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!$%&'()*+,-./:;<=>?@[]^_`{|}~";
+function create() {
+  let complexity = document.getElementById("slider").value;
 
-    let password = "";
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-    // creates a random password full of the characters in line 5
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-    }
+  let password = "";
 
-    document.getElementById("display").value = password;
+  for (var i = 0; i <= complexity; i++) {
+    password =
+      password +
+      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
 
-    document.getElementById("lastPasswords").innerHTML += password + "<br />";
+  document.getElementById("display").value = password;
 
+  document.getElementById("lastPasswords").innerHTML += password + "<br />";
 }
-
 document.getElementById("length").innerHTML = "Length: 128";
 
-document.getElementById("slider").oninput = function(){
+document.getElementById("slider").oninput = function() {
+  if (document.getElementById("slider").value > 0) {
+    document.getElementById("length").innerHTML =
+      "Length: " + document.getElementById("slider").value;
+  } else {
+    document.getElementById("length").innerHTML = "Length: 1";
+  }
+};
 
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+function copyPassword() {
+  document.getElementById("display").select();
+
+  document.execCommand("Copy");
+
+  alert("Password is on clipboard!");
 }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
-    }
+
+function test() {
+  //   var hasUpper = false;
+  //   var hasLower = false;
+  //   var hasInt = false;
+  //   var int = "0,1,2,3,4,5,6,7,8,9,";
+  //   var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  //   var hasSpec = false;
+  var textBox = document.getElementById("display").value;
+  var containsUpper = /[A-Z]/.test(textBox);
+  var containsLower = /[a-z]/.test(textBox);
+  var containsDigits = /[0-9]/.test(textBox);
+  var containsSpecial = /[!@#$%^&*]/.test(textBox);
+  
+
+  if (containsUpper) {
+    document.getElementById("capital").checked = true;
+  }
+
+  if (containsLower) {
+    document.getElementById("lower").checked = true;
+  }
+
+  if (containsDigits) {
+    document.getElementById("numeric").checked = true;
+  }
+
+  if (containsSpecial) {
+    document.getElementById("special").checked = true;
+  }
 
 }
 
-function copyPassword(){
+//     // if (textBox.charAt(i) === textBox.charAt(i).toLowerCase()) {
+//     //   document.getElementById("lower").checked = true;
+//     //   hasLower = true;
+//     // }
 
-    document.getElementById("display").select();
+//     // if (textBox.charAt(i) === (int)) {
+//     //   document.getElementById("numeric").checked = true;
+//     //   hasInt = true;
+//     // }
 
-    document.execCommand("Copy");
-
-    alert("Password on clipboard!");
-}
+//     //     if (textBox.charAt(i) === specialChar) {
+//     //       document.getElementById("special").checked = true;
+//     //       hasSpec = false;
+//     //     }
+//     //   }
+//   }
